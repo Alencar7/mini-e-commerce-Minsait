84882007dd74273179ef9b6dcffbba8e87698b55
Alencar7 <dinhoalencaraa@gmail.com>
2025-12-01 15:33:11 -0300

    feat/rouing: Configura as rotas iniciais(home,listagem(:id,new) e carrinho)
    Implementa o RouterOutlet em app.comp.html e routerlink no header

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductFormComponent } from './product-form/product-form.component';
+import { CartComponent } from './cart/cart.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'products', component: ProductListComponent },
+  { path: 'products/new', component: ProductFormComponent }, // rota para criar um novo produto
+  { path: 'products/edit/:id', component: ProductFormComponent }, // rota para editar um produto existente
+  { path: 'cart', component: CartComponent },
+  { path: '**', redirectTo: '' }, //rota padrão para redirecionar para a página inicial
+];
