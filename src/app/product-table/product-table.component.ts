b926b3313a1e43d3343233fe288b09a3b37d51f2
Alencar7 <dinhoalencaraa@gmail.com>
2025-12-02 19:07:51 -0300

    feat(product-table): Implementa lógica e template da tabela de produtos
    Adiciona as funções necessárias ao ProductTableComponent(.ts e .html)

--- src/app/product-table/product-table.component.ts
+++ src/app/product-table/product-table.component.ts
@@ -1,12 +1,33 @@
 import { Component } from '@angular/core';
+import { Input, Output } from '@angular/core';
+import { EventEmitter } from '@angular/core';
+import { CurrencyPipe } from '@angular/common';
+
+import { Product } from '../models/product.model';
 
 @Component({
   selector: 'app-product-table',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './product-table.component.html',
-  styleUrl: './product-table.component.css'
+  styleUrl: './product-table.component.css',
 })
 export class ProductTableComponent {
+  @Input() products: Product[] = [];
 
+  @Output() edit: EventEmitter<Product> = new EventEmitter<Product>();
+  @Output() delete: EventEmitter<Product> = new EventEmitter<Product>();
+  @Output() addToCart: EventEmitter<Product> = new EventEmitter<Product>();
+
+  onEdit(product: Product) {
+    this.edit.emit(product);
+  }
+
+  onDelete(product: Product) {
+    this.delete.emit(product);
+  }
+
+  onAddToCart(product: Product) {
+    this.addToCart.emit(product);
+  }
 }
